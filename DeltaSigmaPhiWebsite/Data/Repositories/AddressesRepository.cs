1e51697c02eefedbd306dd3f0bf3e575a94d26d0
Ty Morrow <tjm6f4@mst.edu>
2014-06-09 02:57:41 -0500

    Address adding and updating; restyled Account page a bit to fit it.

---
+++ DeltaSigmaPhiWebsite/Data/Repositories/AddressesRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace DeltaSigmaPhiWebsite.Data.Repositories
+{
+    using Interfaces;
+    using Models;
+
+    public class AddressesRepository : GenericRepository<Address>, IAddressesRepository
+    {
+        public AddressesRepository(DspContext context) : base(context)
+        {
+        }
+    }
+}
