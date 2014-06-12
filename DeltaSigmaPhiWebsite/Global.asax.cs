21a838bffce16ab763e5b8b253af2e5617a4c636
Ty Morrow <tjm6f4@mst.edu>
2014-06-12 14:58:30 -0500

    Removed Filter for membership, added explicit call in Global.asax.cs

--- DeltaSigmaPhiWebsite/Global.asax.cs
+++ DeltaSigmaPhiWebsite/Global.asax.cs
@@ -6,11 +6,13 @@
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
+    using WebMatrix.WebData;
 
     public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            WebSecurity.InitializeDatabaseConnection("DefaultConnection", "Members", "UserId", "UserName", true);
             AreaRegistration.RegisterAllAreas();
 
             GlobalConfiguration.Configure(WebApiConfig.Register);
