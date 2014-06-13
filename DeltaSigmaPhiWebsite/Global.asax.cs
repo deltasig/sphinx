2533fa61cb6d20a459bdb9d54463fb36c514328d
Ty Morrow <tjm6f4@mst.edu>
2014-06-13 15:23:44 -0500

    Deployment Configuration Changes

--- DeltaSigmaPhiWebsite/Global.asax.cs
+++ DeltaSigmaPhiWebsite/Global.asax.cs
@@ -1,18 +1,26 @@
 ﻿namespace DeltaSigmaPhiWebsite
 {
+    using System.Data.Entity.Migrations;
     using App_Start;
     using System.Web;
     using System.Web.Http;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
+    using Migrations;
     using WebMatrix.WebData;
 
     public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
-            WebSecurity.InitializeDatabaseConnection("DefaultConnection", "Members", "UserId", "UserName", true);
+            var migrator = new DbMigrator(new Configuration());
+            migrator.Update();
+
+            if (!WebSecurity.Initialized)
+            {
+                WebSecurity.InitializeDatabaseConnection("DefaultConnection", "Members", "UserId", "UserName", true);
+            }
             AreaRegistration.RegisterAllAreas();
 
             GlobalConfiguration.Configure(WebApiConfig.Register);
