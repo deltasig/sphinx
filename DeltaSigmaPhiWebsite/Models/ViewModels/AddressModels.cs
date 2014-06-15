2e5e6ba0f287f1dce390a35fb35b9123314811d9
Ty Morrow <tjm6f4@mst.edu>
2014-06-15 08:03:59 -0500

    Address CRUD refactoring and modal/navbar fix
    Registration creates two address per member. More to come.

---
+++ DeltaSigmaPhiWebsite/Models/ViewModels/AddressModels.cs
@@ -0,0 +1,12 @@
+﻿namespace DeltaSigmaPhiWebsite.Models.ViewModels
+{
+    using Entities;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+
+    public class CreateAddressModel
+    {
+        public IEnumerable<SelectListItem> Users { get; set; }
+        public Address Address { get; set; }
+    }
+}
