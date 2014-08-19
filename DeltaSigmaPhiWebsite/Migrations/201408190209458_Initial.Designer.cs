c0758f65e1f199bdcbebd3d2a59ddc3165b1dec9
Ty Morrow <tyjmorrow@gmail.com>
2014-08-19 00:41:30 -0500

    Bug fixes after deployment.

--- DeltaSigmaPhiWebsite/Migrations/201408190209458_Initial.Designer.cs
+++ DeltaSigmaPhiWebsite/Migrations/201408190209458_Initial.Designer.cs
@@ -6,14 +6,14 @@ namespace DeltaSigmaPhiWebsite.Migrations
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
     
-    [GeneratedCode("EntityFramework.Migrations", "6.1.0-30225")]
+    [GeneratedCode("EntityFramework.Migrations", "6.1.1-30610")]
     public sealed partial class Initial : IMigrationMetadata
     {
         private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201406101402228_Initial"; }
+            get { return "201408190209458_Initial"; }
         }
         
         string IMigrationMetadata.Source
