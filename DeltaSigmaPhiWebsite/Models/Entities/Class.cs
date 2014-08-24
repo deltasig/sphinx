b586646524d2fa565f630c42e009d1c76b8f9903
Ty Morrow <tjm6f4@mst.edu>
2014-08-23 21:14:36 -0500

    Class bug fixes: validation, deletion error message, Edit link.

--- DeltaSigmaPhiWebsite/Models/Entities/Class.cs
+++ DeltaSigmaPhiWebsite/Models/Entities/Class.cs
@@ -19,16 +19,19 @@ namespace DeltaSigmaPhiWebsite.Models.Entities
         [Required]
         [Display(Name = "Number")]
         [DataType(DataType.Text)]
-        [StringLength(7)]
+        [StringLength(15)]
+        [MinLength(1)]
         public string CourseShorthand { get; set; }
 
         [Required]
         [Display(Name = "Name")]
         [StringLength(100)]
+        [MinLength(1)]
         public string CourseName { get; set; }
 
         [Required]
         [Display(Name = "Credit Hours")]
+        [Range(1, 6, ErrorMessage = "Please enter a valid number of credit hours: 1-6.")]
         public int CreditHours { get; set; }
 
         public virtual Department Department { get; set; }
