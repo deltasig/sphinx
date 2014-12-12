b3fe9daf600618f232e47f3acc81aaf13a1fdc20
Ty Morrow <tyjmorrow@gmail.com>
2014-12-12 00:14:29 -0600

    Fixed some missing authorization tags.

--- DeltaSigmaPhiWebsite/Controllers/PositionsController.cs
+++ DeltaSigmaPhiWebsite/Controllers/PositionsController.cs
@@ -7,6 +7,7 @@
     using System.Threading.Tasks;
     using System.Web.Mvc;
 
+    [Authorize(Roles = "Administrator, President")]
     public class PositionsController : Controller
     {
         private readonly DspContext _db = new DspContext();
