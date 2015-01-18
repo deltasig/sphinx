917b2a403fe9b4f5533aed16684a7e7702c0075d
Ty Morrow <tyjmorrow@gmail.com>
2015-01-18 10:41:29 -0600

    Registration semester dropdown bug fix.

--- DeltaSigmaPhiWebsite/Areas/Members/Controllers/AccountController.cs
+++ DeltaSigmaPhiWebsite/Areas/Members/Controllers/AccountController.cs
@@ -178,7 +178,7 @@
                 {
                     StatusList = await GetStatusListAsync(), 
                     PledgeClassList = await GetPledgeClassListAsync(),
-                    SemesterList = await GetSemesterListAsync(),
+                    SemesterList = await GetAllSemesterListAsync(),
                     ShirtSizes = GetShirtSizesSelectList()
                 },
                 UnregisterModel = new UnregisterModel
