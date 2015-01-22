2c24571166c6a788911feeb9646af5b11bc9f0ff
Ty Morrow <tjm6f4@mst.edu>
2015-01-21 22:08:15 -0600

    Enhanced course scheduling features.

--- DeltaSigmaPhiWebsite/Areas/Edu/Models/ClassScheduleModel.cs
+++ DeltaSigmaPhiWebsite/Areas/Edu/Models/ClassScheduleModel.cs
@@ -7,19 +7,11 @@
 
     public class ClassScheduleModel
     {
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "Member")]
-        public int SelectedMember { get; set; }
-        public IEnumerable<SelectListItem> Members { get; set; }
-
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "Semester")]
-        public int SelectedSemester { get; set; }
-        public IEnumerable<SelectListItem> Semesters { get; set; }
+        public string SelectedUserName { get; set; }
+        public ClassTaken ClassTaken { get; set; }
 
         public IEnumerable<Class> AllClasses { get; set; }
-        public List<ClassTaken> ClassesTaken { get; set; }
+        public IEnumerable<SelectListItem> Semesters { get; set; }
+        public IEnumerable<ClassTaken> ClassesTaken { get; set; }
     }
 }
