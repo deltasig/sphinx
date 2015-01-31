5b4fd3c6c5e286daf8fff77ec3409283bf55c409
Josh Jetter <joshuajetter@gmail.com>
2015-01-31 14:56:52 -0800

    Added MealItem CRUD, changed meal area to kitchen

---
+++ DeltaSigmaPhiWebsite/Areas/Kitchen/Controllers/MealItemsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DeltaSigmaPhiWebsite.Entities;
+
+namespace DeltaSigmaPhiWebsite.Areas.Meals.Controllers
+{
+    public class MealItemsController : Controller
+    {
+        private DspDbContext db = new DspDbContext();
+
+        // GET: Meals/Items
+        public async Task<ActionResult> Index()
+        {
+            var mealItems = db.MealItems.Include(m => m.MealItemType);
+            return View(await mealItems.ToListAsync());
+        }
+
+        // GET: Meals/Items/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MealItem mealItem = await db.MealItems.FindAsync(id);
+            if (mealItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mealItem);
+        }
+
+        // GET: Meals/Items/Create
+        public ActionResult Create()
+        {
+            ViewBag.MealItemTypeId = new SelectList(db.MealItemTypes, "MealItemTypeId", "Name");
+            return View();
+        }
+
+        // POST: Meals/Items/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "MealItemId,MealItemTypeId,Name,IsGlutenFree")] MealItem mealItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MealItems.Add(mealItem);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MealItemTypeId = new SelectList(db.MealItemTypes, "MealItemTypeId", "Name", mealItem.MealItemTypeId);
+            return View(mealItem);
+        }
+
+        // GET: Meals/Items/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MealItem mealItem = await db.MealItems.FindAsync(id);
+            if (mealItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MealItemTypeId = new SelectList(db.MealItemTypes, "MealItemTypeId", "Name", mealItem.MealItemTypeId);
+            return View(mealItem);
+        }
+
+        // POST: Meals/Items/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "MealItemId,MealItemTypeId,Name,IsGlutenFree")] MealItem mealItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mealItem).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MealItemTypeId = new SelectList(db.MealItemTypes, "MealItemTypeId", "Name", mealItem.MealItemTypeId);
+            return View(mealItem);
+        }
+
+        // GET: Meals/Items/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MealItem mealItem = await db.MealItems.FindAsync(id);
+            if (mealItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mealItem);
+        }
+
+        // POST: Meals/Items/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            MealItem mealItem = await db.MealItems.FindAsync(id);
+            db.MealItems.Remove(mealItem);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- DeltaSigmaPhiWebsite/Areas/Kitchen/Controllers/MealsController.cs
+++ DeltaSigmaPhiWebsite/Areas/Kitchen/Controllers/MealsController.cs
@@ -11,92 +11,107 @@ using DeltaSigmaPhiWebsite.Entities;
 
 namespace DeltaSigmaPhiWebsite.Areas.Meals.Controllers
 {
-    public class ItemTypesController : Controller
+    public class MealsController : Controller
     {
         private DspDbContext db = new DspDbContext();
 
-        // GET: Meals/ItemTypes
+        // GET: Meals/Meals
         public async Task<ActionResult> Index()
         {
-            return View(await db.MealItemTypes.Include(m => m.MealItems).ToListAsync());
+            return View(await db.Meals.ToListAsync());
         }
 
-        // GET: Meals/ItemTypes/Create
+        // GET: Meals/Meals/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Meal meal = await db.Meals.FindAsync(id);
+            if (meal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(meal);
+        }
+
+        // GET: Meals/Meals/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Meals/ItemTypes/Create
+        // POST: Meals/Meals/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "MealItemTypeId,Name")] MealItemType mealItemType)
+        public async Task<ActionResult> Create([Bind(Include = "MealId")] Meal meal)
         {
             if (ModelState.IsValid)
             {
-                db.MealItemTypes.Add(mealItemType);
+                db.Meals.Add(meal);
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
 
-            return View(mealItemType);
+            return View(meal);
         }
 
-        // GET: Meals/ItemTypes/Edit/5
+        // GET: Meals/Meals/Edit/5
         public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            MealItemType mealItemType = await db.MealItemTypes.FindAsync(id);
-            if (mealItemType == null)
+            Meal meal = await db.Meals.FindAsync(id);
+            if (meal == null)
             {
                 return HttpNotFound();
             }
-            return View(mealItemType);
+            return View(meal);
         }
 
-        // POST: Meals/ItemTypes/Edit/5
+        // POST: Meals/Meals/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "MealItemTypeId,Name")] MealItemType mealItemType)
+        public async Task<ActionResult> Edit([Bind(Include = "MealId")] Meal meal)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(mealItemType).State = EntityState.Modified;
+                db.Entry(meal).State = EntityState.Modified;
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            return View(mealItemType);
+            return View(meal);
         }
 
-        // GET: Meals/ItemTypes/Delete/5
+        // GET: Meals/Meals/Delete/5
         public async Task<ActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            MealItemType mealItemType = await db.MealItemTypes.FindAsync(id);
-            if (mealItemType == null)
+            Meal meal = await db.Meals.FindAsync(id);
+            if (meal == null)
             {
                 return HttpNotFound();
             }
-            return View(mealItemType);
+            return View(meal);
         }
 
-        // POST: Meals/ItemTypes/Delete/5
+        // POST: Meals/Meals/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(int id)
         {
-            MealItemType mealItemType = await db.MealItemTypes.FindAsync(id);
-            db.MealItemTypes.Remove(mealItemType);
+            Meal meal = await db.Meals.FindAsync(id);
+            db.Meals.Remove(meal);
             await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
