859bde8726658c3f61525f488c40b9c79d42a8a7
Ty Morrow <tjm6f4@mst.edu>
2015-02-10 16:38:53 -0600

    Added multi sober shift tool.

---
+++ DeltaSigmaPhiWebsite/Areas/Sphinx/Models/MultiAddSoberSignupModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DeltaSigmaPhiWebsite.Areas.Sphinx.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class MultiAddSoberSignupModel
+    {
+        [Range(0, 5)]
+        public int DriverAmount { get; set; }
+        [Range(0, 5)]
+        public int OfficerAmount { get; set; }
+        public string DateString { get; set; }
+    }
+}
