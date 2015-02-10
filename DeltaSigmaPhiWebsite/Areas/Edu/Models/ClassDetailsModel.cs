96e4d0c9d07c5f9bc1d20505cc0039fd8196793e
Ty Morrow <tyjmorrow@gmail.com>
2015-02-10 17:33:00 -0600

    Added class details because of popover bug.

---
+++ DeltaSigmaPhiWebsite/Areas/Edu/Models/ClassDetailsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DeltaSigmaPhiWebsite.Areas.Edu.Models
+{
+    using Entities;
+
+    public class ClassDetailsModel
+    {
+        public Class Class { get; set; }
+        public Semester CurrentSemester { get; set; }
+    }
+}
