2ab7e7c524d54a0732003161b21a8bd011884781
Ty Morrow <tjm6f4@mst.edu>
2015-02-13 01:27:02 -0600

    Added initial support for electronic file system.

---
+++ DeltaSigmaPhiWebsite/Areas/Edu/Models/FileInfoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DeltaSigmaPhiWebsite.Areas.Edu.Models
+{
+    using System.Collections.Generic;
+    using System.Web;
+
+    public class FileInfoModel
+    {
+        public HttpPostedFileBase File { get; set; }
+        public IEnumerable<string> ExistingFiles { get; set; }
+    }
+}
