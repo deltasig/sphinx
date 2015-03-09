c0c959ee727b0ac8dfe684fb22ea61b030b02e2a
Ty Morrow <tjm6f4@mst.edu>
2015-03-08 23:53:53 -0500

    Authorization annotation for rooms controller.

--- DeltaSigmaPhiWebsite/Areas/House/Controllers/RoomsController.cs
+++ DeltaSigmaPhiWebsite/Areas/House/Controllers/RoomsController.cs
@@ -3,6 +3,7 @@
 	using System.Web.Mvc;
 	using DeltaSigmaPhiWebsite.Controllers;
 
+	[Authorize(Roles = "Pledge, Neophyte, Active, Alumnus, Affiliate")]
 	public class RoomsController : BaseController
 	{
 		public ActionResult Index()
