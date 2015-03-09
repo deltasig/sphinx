a8e54a4a11fc6da55294520b7ef4aa4f2e3aefe9
Ty Morrow <tjm6f4@mst.edu>
2015-03-08 21:56:16 -0500

    Added Housing Area, Rooms controller, and second floor svg.

---
+++ DeltaSigmaPhiWebsite/Areas/Housing/Controllers/RoomsController.cs
@@ -0,0 +1,13 @@
+﻿namespace DeltaSigmaPhiWebsite.Areas.Housing.Controllers
+{
+	using System.Web.Mvc;
+	using DeltaSigmaPhiWebsite.Controllers;
+
+	public class RoomsController : BaseController
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
