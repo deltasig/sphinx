6865f71295d297a24dc65597ae97b440fc17def4
Ty Morrow <tjm6f4@mst.edu>
2015-04-08 16:12:54 -0500

    Refactored service hours table footer.

--- DeltaSigmaPhiWebsite/Areas/Service/Models/ServiceHourIndexFilterModel.cs
+++ DeltaSigmaPhiWebsite/Areas/Service/Models/ServiceHourIndexFilterModel.cs
@@ -2,11 +2,16 @@
 {
     using System.Collections.Generic;
     using System.Web.Mvc;
+    using Entities;
 
     public class ServiceHourIndexFilterModel
     {
         public List<ServiceHourIndexModel> ServiceHours { get; set; }
         public int? SelectedSemester { get; set; }
         public IEnumerable<SelectListItem> SemesterList { get; set; }
+
+        public Semester Semester { get; set; }
+
+        public Semester PreviousSemester { get; set; }
     }
 }
