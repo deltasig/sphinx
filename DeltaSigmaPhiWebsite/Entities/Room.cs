549282cd738060e0499823ac6c683b8925c29551
Ty Morrow <tjm6f4@mst.edu>
2015-04-22 20:11:25 -0500

    Added Rooms entities.

---
+++ DeltaSigmaPhiWebsite/Entities/Room.cs
@@ -0,0 +1,19 @@
+﻿namespace DeltaSigmaPhiWebsite.Entities
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Room
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RoomId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public virtual ICollection<RoomToMember> Members { get; set; }
+    }
+}
