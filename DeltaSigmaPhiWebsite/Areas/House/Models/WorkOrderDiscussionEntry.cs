bfb88c17fefe9d2cbd451faad6b8a757a21a1e7a
Ty Morrow <tyjmorrow@gmail.com>
2015-05-04 09:24:42 -0500

    Refactored work order view discussion.

--- DeltaSigmaPhiWebsite/Areas/House/Models/WorkOrderDiscussionEntry.cs
+++ DeltaSigmaPhiWebsite/Areas/House/Models/WorkOrderDiscussionEntry.cs
@@ -7,6 +7,7 @@
         public DateTime OccurredOn { get; set; }
         public int UserId { get; set; }
         public string Name { get; set; }
+        public string Title { get; set; }
         public string Text { get; set; }
     }
 }
