fc3824a8c35dfeb0154346da4689fae04bf67a43
Ty Morrow <tjm6f4@mst.edu>
2015-08-08 19:21:41 -0500

    Added profile picture support.

--- Dsp/Areas/House/Models/WorkOrderDiscussionEntry.cs
+++ Dsp/Areas/House/Models/WorkOrderDiscussionEntry.cs
@@ -9,5 +9,6 @@
         public string Name { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
+        public string AvatarPath { get; set; }
     }
 }
