1cf9d5c915560bda46697145f7e51a8f3a76c188
Ty Morrow <tjm6f4@mst.edu>
2015-08-15 22:38:36 -0500

    Added news feed to sphinx page.

---
+++ Dsp/Areas/Sphinx/Models/FeedItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Dsp.Areas.Sphinx.Models
+{
+    using System;
+
+    public class FeedItem
+    {
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string ImageName { get; set; }
+        public int DaysSince { get; set; }
+        public DateTime OccurredOn { get; set; }
+        public string DisplayText { get; set; }
+        public string Link { get; set; }
+        public string Symbol { get; set; }
+    }
+}
