58a51f1e12a6a0d31eb9ebff4e906bd149fb68a9
Ty Morrow <tjm6f4@mst.edu>
2015-08-16 14:27:29 -0500

    Improved error logs page with paging and details.

--- Dsp/Controllers/ErrorController.cs
+++ Dsp/Controllers/ErrorController.cs
@@ -1,13 +1,15 @@
 ﻿namespace Dsp.Controllers
 {
+    using Data;
+    using System;
     using System.Collections.Generic;
     using System.Data.Entity;
-    using Elmah;
+    using System.IO;
+    using System.Linq;
     using System.Net;
     using System.Threading.Tasks;
-    using System.Web.Configuration;
     using System.Web.Mvc;
-    using Data;
+    using System.Xml;
 
     [Authorize(Roles = "Administrator")]
     public class ErrorController : Controller
@@ -34,16 +36,57 @@
             return View("InternalServerError");
         }
 
-        public async Task<ActionResult> Logs()
+        public async Task<ActionResult> Logs(int page = 1)
         {
-            List<ElmahErrorLog> errors;
-            
+            List<ElmahErrorLog> logs;
             using (var db = new ElmahDbContext())
             {
-                errors = await db.Errors.ToListAsync();
-            }
+                const int pageSize = 10;
+                var logsCount = await db.Errors.CountAsync();
+                var pageCount = logsCount / pageSize;
+                // Make sure no improper page values were entered
+                page = page < 1 ? 1 : page;
+                page = page > pageCount && pageCount > 0 ? pageCount : page;
+                // Set ViewBag properties for paging (required for pager to function properly)
+                ViewBag.Page = page;
+                ViewBag.PageSize = pageSize;
+                ViewBag.Count = logsCount;
+                ViewBag.Pages = pageCount;
 
-            return View(errors);
+                // Load logs excluding details because the details are big XML blobs
+                logs = (await db.Errors
+                    .OrderByDescending(e => e.TimeUtc)
+                    .Skip(pageSize * (page - 1))
+                    .Take(pageSize)
+                    .Select(e => new { e.ErrorId, e.StatusCode, e.User, e.Type, e.Source, e.Message, e.TimeUtc }).ToListAsync())
+                    .Select(e => new ElmahErrorLog
+                    {
+                        ErrorId = e.ErrorId,
+                        StatusCode = e.StatusCode,
+                        User = e.User,
+                        Type = e.Type,
+                        Source = e.Source,
+                        Message = e.Message,
+                        TimeUtc = e.TimeUtc
+                    }).ToList();
+            }
+            return View(logs);
+        }
+
+        public async Task<JsonResult> GetLogDetails(Guid id)
+        {
+            string data;
+            using (var db = new ElmahDbContext())
+            {
+                var log = await db.Errors.FindAsync(id);
+                using (var reader = XmlReader.Create(new StringReader(log.AllXml)))
+                {
+                    reader.ReadToFollowing("error");
+                    reader.MoveToAttribute("detail");
+                    data = reader.Value;
+                }
+            }
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
