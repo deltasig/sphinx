a7f18143addd66c1a2d91909307cb4881cdcdf95
Ty Morrow <tjm6f4@mst.edu>
2015-12-08 15:11:47 -0600

    Fixed timespan extension bug.

--- Dsp/Extensions/TimeSpanExtensions.cs
+++ Dsp/Extensions/TimeSpanExtensions.cs
@@ -14,46 +14,49 @@
             {
                 output.Append("just now");
             }
-            else if(timeSpan.TotalMinutes < 60)
-            {
-                value = (int)timeSpan.TotalMinutes;
-                output.Append(value);
-                output.Append(" minute");
-            }
-            else if(timeSpan.TotalHours < 24)
-            {
-                value = (int)timeSpan.TotalHours;
-                output.Append(value);
-                output.Append(" hour");
-            }
-            else if (timeSpan.TotalDays < 7)
-            {
-                value = (int)timeSpan.TotalDays;
-                output.Append(value);
-                output.Append(" day");
-            }
-            else if (timeSpan.TotalDays < 31)
-            {
-                value = (int)timeSpan.TotalDays / 7;
-                output.Append(value);
-                output.Append(" week");
-            }
-            else if (timeSpan.TotalDays < 365)
-            {
-                value = (int)timeSpan.TotalDays / 31;
-                output.Append(value);
-                output.Append(" month");
-            }
             else
             {
-                value = (int)timeSpan.TotalDays / 365;
-                output.Append(value);
-                output.Append(" year");
+                if (timeSpan.TotalMinutes < 60)
+                {
+                    value = (int)timeSpan.TotalMinutes;
+                    output.Append(value);
+                    output.Append(" minute");
+                }
+                else if (timeSpan.TotalHours < 24)
+                {
+                    value = (int)timeSpan.TotalHours;
+                    output.Append(value);
+                    output.Append(" hour");
+                }
+                else if (timeSpan.TotalDays < 7)
+                {
+                    value = (int)timeSpan.TotalDays;
+                    output.Append(value);
+                    output.Append(" day");
+                }
+                else if (timeSpan.TotalDays < 31)
+                {
+                    value = (int)timeSpan.TotalDays / 7;
+                    output.Append(value);
+                    output.Append(" week");
+                }
+                else if (timeSpan.TotalDays < 365)
+                {
+                    value = (int)timeSpan.TotalDays / 31;
+                    output.Append(value);
+                    output.Append(" month");
+                }
+                else
+                {
+                    value = (int)timeSpan.TotalDays / 365;
+                    output.Append(value);
+                    output.Append(" year");
+                }
+                if (value != 1)
+                    output.Append("s");
+                output.Append(" ago");
             }
 
-            if (value != 1)
-                output.Append("s");
-            output.Append(" ago");
             return output.ToString();
         }
     }
