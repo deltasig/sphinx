fc7fe4e9eb318824b451a3d90811aeb3bdaca6c3
Ty Morrow <tjm6f4@mst.edu>
2016-07-23 22:33:57 -0600

    Added BroQuest entities.

---
+++ src/Dsp.Data/Entities/QuestCompletion.cs
@@ -0,0 +1,27 @@
+﻿namespace Dsp.Data.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    /// <summary>
+    /// Represents a completed challenge by a new member.
+    /// </summary>
+    public class QuestCompletion
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, Column(Order = 0), Index("IX_QuestCompletion", 0, IsUnique = true)]
+        public int ChallengeId { get; set; }
+
+        [Required, Column(Order = 1), Index("IX_QuestCompletion", 1, IsUnique = true)]
+        public int NewMemberId { get; set; }
+
+        public bool IsVerified { get; set; }
+
+        [ForeignKey("ChallengeId")]
+        public virtual QuestChallenge Challenge { get; set; }
+        [ForeignKey("NewMemberId")]
+        public virtual Member Member { get; set; }
+    }
+}
