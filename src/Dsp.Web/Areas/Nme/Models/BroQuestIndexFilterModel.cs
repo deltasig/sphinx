96a210dde84a5acbdaa30c94be899b118b31fe2e
Ty Morrow <tyjmorrow@gmail.com>
2016-07-24 19:37:30 -0600

    Added initial broquest index page.

---
+++ src/Dsp.Web/Areas/Nme/Models/BroQuestIndexFilterModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Dsp.Web.Areas.Nme.Models
+{
+    using Dsp.Web.Models;
+
+    public class BroQuestIndexFilterModel
+    {
+        public int? s { get; set; }
+        public bool i { get; set; }
+        public bool c { get; set; }
+
+        public BroQuestIndexFilterModel()
+        {
+            
+        }
+        public BroQuestIndexFilterModel(BroQuestIndexFilterModel original)
+        {
+            s = original.s;
+            i = original.i;
+            c = original.c;
+        }
+    }
+}
--- src/Dsp.Web/Areas/Nme/Models/BroQuestIndexModel.cs
+++ src/Dsp.Web/Areas/Nme/Models/BroQuestIndexModel.cs
@@ -8,10 +8,21 @@
     {
         public IEnumerable<SelectListItem> SemesterList { get; set; }
         public Semester Semester { get; set; }
+        public Member Member { get; set; }
+        public IEnumerable<Member> Members { get; set; }
 
         public BroQuestIndexModel(Semester semester)
         {
             Semester = semester;
+        }
+        public BroQuestIndexModel(Member member)
+        {
+            Member = member;
+        }
+        public BroQuestIndexModel(Semester semester, Member member)
+        {
+            Semester = semester;
+            Member = member;
         }
     }
 }
