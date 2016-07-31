eb257616514a014d53f086e62fb23a80e3093a7e
Ty Morrow <tjm6f4@mst.edu>
2016-07-30 20:16:35 -0600

    BroQuest Challenge Adding/Deleting.

--- src/Dsp.Data/Entities/QuestChallenge.cs
+++ src/Dsp.Data/Entities/QuestChallenge.cs
@@ -12,14 +12,18 @@
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        
+
+        [Required, Column(Order = 0), Index("IX_QuestChallenge", 0, IsUnique = true)]
         public int SemesterId { get; set; }
-        
+
+        [Required, Column(Order = 1), Index("IX_QuestChallenge", 1, IsUnique = true)]
         public int MemberId { get; set; }
 
+        [Required, Column(Order = 2), Index("IX_QuestChallenge", 2, IsUnique = true)]
         [Display(Name = "Begins On")]
         public DateTime? BeginsOn { get; set; }
 
+        [Required, Column(Order = 3), Index("IX_QuestChallenge", 3, IsUnique = true)]
         [Display(Name = "Ends On")]
         public DateTime? EndsOn { get; set; }
 
