01b1a0433fe2247ebdffd324db07e1d1e9bb255e
Ty Morrow <tyjmorrow@gmail.com>
2017-01-14 16:50:33 -0600

    Moved sphinx notifications to tab with feed.

---
+++ src/Dsp.Web/Areas/Sphinx/Models/Notification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsp.Web.Areas.Sphinx.Models
+{
+    public class Notification
+    {
+        public string Message { get; set; }
+        public string LinkText { get; set; }
+        public string Link { get; set; }
+        public string Why { get; set; }
+        public NotificationPriority Priority { get; set; }
+    }
+
+    public enum NotificationPriority
+    {
+        Low,
+        Moderate,
+        High
+    }
+}
