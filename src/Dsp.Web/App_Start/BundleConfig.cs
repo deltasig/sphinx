3021213e92137563fc5aff3b40b07a6df95e5e39
Ty Morrow <tjm6f4@mst.edu>
2017-01-14 18:42:29 -0600

    Templating and improving sphinx page notifications and feed.

--- src/Dsp.Web/App_Start/BundleConfig.cs
+++ src/Dsp.Web/App_Start/BundleConfig.cs
@@ -30,6 +30,10 @@
             bundles.Add(new ScriptBundle("~/bundles/bootstrap-core")
                 .Include("~/Scripts/bootstrap.js")
                 .Include("~/Scripts/bootstrap-select.js"));
+            bundles.Add(new ScriptBundle("~/bundles/moment")
+                .Include("~/Scripts/moment.js"));
+            bundles.Add(new ScriptBundle("~/bundles/mustache")
+                .Include("~/Scripts/mustache.js"));
             bundles.Add(new ScriptBundle("~/bundles/jquery")
                 .Include("~/Scripts/jquery-{version}.js")
                 .Include("~/Scripts/jquery-ui-{version}.js")
