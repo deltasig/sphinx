7f447563b490c45f5390427f74017795e6dc4683
Ty Morrow <tyjmorrow@gmail.com>
2017-09-18 19:35:48 -0600

    Added MealToPeriod index.

--- src/Dsp.Data/Entities/MealToPeriod.cs
+++ src/Dsp.Data/Entities/MealToPeriod.cs
@@ -10,11 +10,13 @@
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int MealToPeriodId { get; set; }
 
+        [Required, Column(Order = 0), Index("IX_MealToPeriod", 0, IsUnique = true)]
         public int MealPeriodId { get; set; }
 
+        [Required, Column(Order = 1), Index("IX_MealToPeriod", 1, IsUnique = true)]
         public int MealId { get; set; }
 
-        [Required, Column(TypeName="Date")]
+        [Required, Column(Order = 2, TypeName="Date"), Index("IX_MealToPeriod", 2, IsUnique = true)]
         public DateTime Date { get; set; }
 
         [ForeignKey("MealPeriodId")]
