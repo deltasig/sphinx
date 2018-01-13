c0d978ee91d63f59f2ace46dc978f7a24d4baa5f
Ty Morrow <tyjmorrow@gmail.com>
2018-01-13 16:36:08 -0700

    Partial sober service impimplementation.

--- src/Dsp.Web/Areas/Sobers/Controllers/ScheduleController.cs
+++ src/Dsp.Web/Areas/Sobers/Controllers/ScheduleController.cs
@@ -1,32 +1,42 @@
 ﻿namespace Dsp.Web.Areas.Sobers.Controllers
 {
     using Data.Entities;
-    using Web.Controllers;
-    using Extensions;
-    using Microsoft.AspNet.Identity;
+    using Dsp.Data;
+    using Dsp.Repositories;
+    using Dsp.Services;
+    using Dsp.Services.Interfaces;
     using Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Linq;
     using System.Net;
-    using System.Net.Mail;
     using System.Text;
     using System.Threading.Tasks;
     using System.Web.Mvc;
+    using Web.Controllers;
 
     [Authorize(Roles = "Pledge, Neophyte, Active, Alumnus, Affiliate")]
     public class ScheduleController : BaseController
     {
+        private ISoberService _soberService;
+
+        public ScheduleController()
+        {
+            _soberService = new SoberService(new Repository<SphinxDbContext>(_db));
+        }
+
+        public ScheduleController(ISoberService soberService)
+        {
+            _soberService = soberService;
+        }
+
         public async Task<ActionResult> Index(SoberMessage? message)
         {
             SetSoberMessage(message);
 
-            var threeAmYesterday = ConvertCstToUtc(ConvertUtcToCst(DateTime.UtcNow).Date).AddDays(-1).AddHours(3);
+            var signups = await _soberService.GetAllFutureSignupsAsync();
 
-            var signups = await _db.SoberSignups
-                .Where(s => s.DateOfShift >= threeAmYesterday)
-                .OrderBy(s => s.DateOfShift)
-                .ToListAsync();
             return View(signups);
         }
 
@@ -35,17 +45,11 @@
         {
             SetSoberMessage(message);
 
-            var startOfTodayUtc = ConvertCstToUtc(ConvertUtcToCst(DateTime.UtcNow).Date);
-            var vacantSignups = await _db.SoberSignups
-                .Where(s => s.DateOfShift >= startOfTodayUtc &&
-                            s.UserId == null)
-                .OrderBy(s => s.DateOfShift)
-                .Include(s => s.SoberType)
-                .ToListAsync();
+            var soberTypes = await _soberService.GetTypesAsync();
             var model = new SoberManagerModel
             {
-                Signups = vacantSignups,
-                SignupTypes = await GetSoberTypesSelectList(),
+                Signups = await _soberService.GetFutureVacantSignups(),
+                SignupTypes = GetSoberTypesSelectList(soberTypes),
                 NewSignup = new SoberSignup(),
                 MultiAddModel = new MultiAddSoberSignupModel
                 {
@@ -65,8 +69,7 @@
             model.NewSignup.DateOfShift = ConvertCstToUtc(model.NewSignup.DateOfShift);
             model.NewSignup.CreatedOn = DateTime.UtcNow;
 
-            _db.SoberSignups.Add(model.NewSignup);
-            await _db.SaveChangesAsync();
+            await _soberService.AddSignupAsync(model.NewSignup);
 
             return RedirectToAction("Manager", new { message = SoberMessage.AddSignupSuccess });
         }
@@ -74,9 +77,14 @@
         [Authorize(Roles = "Administrator, Sergeant-at-Arms")]
         public async Task<ActionResult> MultiAddSignup(SoberManagerModel model)
         {
-            if ((model.MultiAddModel.DriverAmount == 0 && model.MultiAddModel.OfficerAmount == 0) ||
-                string.IsNullOrEmpty(model.MultiAddModel.DateString))
+            var modelInvalid =
+                (model.MultiAddModel.DriverAmount == 0 &&
+                 model.MultiAddModel.OfficerAmount == 0) ||
+                string.IsNullOrEmpty(model.MultiAddModel.DateString);
+            if (modelInvalid)
+            {
                 return RedirectToAction("Manager", new { message = SoberMessage.MultiAddSignupFailure });
+            }
 
             var dateStrings = model.MultiAddModel.DateString.Split(',');
             var driverType = await _db.SoberTypes.SingleOrDefaultAsync(t => t.Name == "Driver");
@@ -87,12 +95,16 @@
                 return RedirectToAction("Manager", new { message = SoberMessage.MultiAddSignupMissingTypesFailure });
             }
 
-            if (dateStrings.Any())
+            if (!dateStrings.Any())
             {
+                return RedirectToAction("Manager", new { message = SoberMessage.MultiAddSignupNoDatesFailure });
+            }
+            else
+            {
+                var signups = new List<SoberSignup>();
                 foreach (var s in dateStrings)
                 {
-                    DateTime date;
-                    var parsed = DateTime.TryParse(s, out date);
+                    var parsed = DateTime.TryParse(s, out DateTime date);
                     if (!parsed) continue;
                     var utcDate = new DateTime(date.Year, date.Month, date.Day, 0, 0, 0, DateTimeKind.Utc);
                     var cstDate = ConvertUtcToCst(utcDate);
@@ -101,7 +113,7 @@
 
                     for (var i = 0; i < model.MultiAddModel.DriverAmount; i++)
                     {
-                        _db.SoberSignups.Add(new SoberSignup
+                        signups.Add(new SoberSignup
                         {
                             DateOfShift = date,
                             SoberTypeId = driverType.SoberTypeId,
@@ -110,7 +122,7 @@
                     }
                     for (var i = 0; i < model.MultiAddModel.OfficerAmount; i++)
                     {
-                        _db.SoberSignups.Add(new SoberSignup
+                        signups.Add(new SoberSignup
                         {
                             DateOfShift = date,
                             SoberTypeId = officerType.SoberTypeId,
@@ -118,39 +130,35 @@
                         });
                     }
                 }
+                await _soberService.AddSignupsAsync(signups);
             }
-
-            await _db.SaveChangesAsync();
 
             return RedirectToAction("Manager", new { message = SoberMessage.MultiAddSignupSuccess });
         }
 
         [Authorize(Roles = "Administrator, Sergeant-at-Arms")]
-        public async Task<ActionResult> DeleteSignup(int? id, string returnUrl)
+        public async Task<ActionResult> DeleteSignup(int id, string returnUrl)
         {
-            if (id == null)
+            if (id <= 0) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            try
+            {
+                await _soberService.DeleteSignupAsync(id);
+
+                if (string.IsNullOrEmpty(returnUrl))
+                    return RedirectToAction("Index", new { message = SoberMessage.DeleteSignupSuccess });
+                return Redirect(returnUrl);
+            }
+            catch
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-
-            var model = await _db.SoberSignups.FindAsync(id);
-
-            if (model == null) return HttpNotFound();
-
-            _db.SoberSignups.Remove(model);
-            await _db.SaveChangesAsync();
-
-            if (string.IsNullOrEmpty(returnUrl))
-                return RedirectToAction("Index", new { message = SoberMessage.DeleteSignupSuccess });
-            return Redirect(returnUrl);
         }
 
-        public async Task<ActionResult> SignupConfirmation(int? id)
+        public async Task<ActionResult> SignupConfirmation(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+            if (id <= 0) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
             var model = await _db.SoberSignups.FindAsync(id);
 
             if (model == null) return HttpNotFound();
@@ -294,6 +302,20 @@
             }
 
             return File(new UTF8Encoding().GetBytes(sb.ToString()), "text/csv", "DSP Sober Signups - " + semester + ".csv");
+        }
+
+        private SelectList GetSoberTypesSelectList(IEnumerable<SoberType> types)
+        {
+            var newList = new List<object>();
+            foreach (var m in types)
+            {
+                newList.Add(new
+                {
+                    m.SoberTypeId,
+                    Text = m.Name
+                });
+            }
+            return new SelectList(newList, "SoberTypeId", "Text");
         }
     }
 }
