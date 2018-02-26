fb4d07e65c1434d423443bbeff43ab0e13fa497f
Ty Morrow <tyjmorrow@gmail.com>
2018-02-25 21:36:10 -0700

    Removed Laundry Statistics - closes #40.

--- src/Dsp.Web/Areas/Laundry/Controllers/ScheduleController.cs
+++ src/Dsp.Web/Areas/Laundry/Controllers/ScheduleController.cs
@@ -1,14 +1,11 @@
 ﻿namespace Dsp.Web.Areas.Laundry.Controllers
 {
-    using Dsp.Web.Controllers;
     using Dsp.Data.Entities;
+    using Dsp.Web.Controllers;
     using Microsoft.AspNet.Identity;
     using Models;
-    using Newtonsoft.Json;
     using System;
-    using System.Collections.Generic;
     using System.Data.Entity;
-    using System.Globalization;
     using System.Linq;
     using System.Threading.Tasks;
     using System.Web.Mvc;
@@ -31,9 +28,9 @@
             var allSemesters = await _db.Semesters.ToListAsync();
             var thisSemester = await GetThisSemesterAsync();
             var semesters = allSemesters
-                .Where(s => 
-                    signups.Any(i => 
-                        i.DateTimeShift >= s.DateStart && 
+                .Where(s =>
+                    signups.Any(i =>
+                        i.DateTimeShift >= s.DateStart &&
                         i.DateTimeShift <= s.DateEnd))
                 .ToList();
             // Sometimes the current semester doesn't contain any signups, yet we still want it in the list
@@ -118,158 +115,5 @@
             TempData["SuccessMessage"] = "You cancelled your reservation for: " + shift.DateTimeShift;
             return RedirectToAction("Index");
         }
-
-        [HttpGet, AllowAnonymous]
-        public async Task<ActionResult> GetStats(int? sid)
-        {
-            var model = new LaundryStatsModel();
-            var semester = await _db.Semesters.FindAsync(sid) ?? await GetThisSemesterAsync();
-            var signups = await _db.LaundrySignups
-                .Where(l => l.DateTimeShift >= semester.DateStart && l.DateTimeShift <= semester.DateEnd)
-                .OrderBy(l => l.DateTimeShift)
-                .ToListAsync();
-
-            var formattedSignups = signups.Select(l => new
-            {
-                DateTimeShift = ConvertUtcToCst(l.DateTimeShift),
-                DateTimeSignedUp = ConvertUtcToCst(l.DateTimeSignedUp),
-                l.UserId
-            }).ToList();
-
-            // Build model
-            model.TotalSignups = formattedSignups.Count();
-            model.Semester = semester.ToString();
-            model.StartDate = ConvertUtcToCst(semester.DateStart).ToShortDateString();
-            model.EndDate = ConvertUtcToCst(semester.DateEnd).ToShortDateString();
-
-            // Month Calculations
-            var months = MonthsBetween(semester.DateStart, semester.DateEnd).OrderBy(m => m.Month).ToList();
-            var monthValues = months.Select(m => m.Month).Distinct();
-            model.MonthChartXLabels = months.Select(m => CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(m.Month)).ToList();
-            model.MonthChartXValues = new List<int>();
-            foreach (var m in monthValues)
-            {
-                model.MonthChartXValues.Add(signups.Count(s => s.DateTimeShift.Month == m));
-            }
-            model.MonthAverage = (decimal)model.TotalSignups / months.Count;
-
-            // Week Calculations
-            var startWeek = GetIso8601WeekOfYear(semester.DateStart);
-            var endWeek = GetIso8601WeekOfYear(semester.DateEnd);
-            var totalWeeks = endWeek - startWeek;
-            model.WeekChartXLabels = new List<string>(GetDayNames());
-            model.WeekChartXValues = new List<decimal>();
-            for (var w = 0; w <= 6; w++)
-            {
-                var averageSignups = (decimal)signups.Count(s => (int)s.DateTimeShift.DayOfWeek == w) / totalWeeks;
-                model.WeekChartXValues.Add(decimal.Round(averageSignups, 2, MidpointRounding.AwayFromZero));
-            }
-            model.WeekAverage = decimal.Round((decimal)model.TotalSignups / totalWeeks, 2, MidpointRounding.AwayFromZero);
-
-            var json = JsonConvert.SerializeObject(model, Formatting.Indented);
-            return Json(json, JsonRequestBehavior.AllowGet);
-        }
-
-        [HttpGet, AllowAnonymous]
-        public async Task<ActionResult> Usage()
-        {
-            // Get semester list
-            var signups = await _db.LaundrySignups.ToListAsync();
-            var allSemesters = await _db.Semesters.ToListAsync();
-            var thisSemester = await GetThisSemesterAsync();
-            var semesters = allSemesters
-                .Where(s => 
-                    signups.Any(i => 
-                        i.DateTimeShift >= s.DateStart && 
-                        i.DateTimeShift <= s.DateEnd))
-                .ToList();
-            // Sometimes the current semester doesn't contain any signups, yet we still want it in the list
-            if (semesters.All(s => s.SemesterId != thisSemester.SemesterId))
-            {
-                semesters.Add(thisSemester);
-            }
-
-            return View(GetCustomSemesterListAsync(semesters));
-        }
-
-        [HttpGet, AllowAnonymous]
-        public async Task<ActionResult> GetUsageStats(int? sid)
-        {
-            var model = new LaundryUsageStatsModel();
-
-            var semester = await _db.Semesters.FindAsync(sid) ?? await GetThisSemesterAsync();
-
-            var signups = await _db.LaundrySignups
-                .Where(l => l.DateTimeShift >= semester.DateStart && l.DateTimeShift <= semester.DateEnd)
-                .OrderBy(l => l.DateTimeShift)
-                .ToListAsync();
-
-            var formattedSignups = signups.Select(l => new
-            {
-                DateTimeShift = ConvertUtcToCst(l.DateTimeShift),
-                DateTimeSignedUp = ConvertUtcToCst(l.DateTimeSignedUp),
-                l.UserId
-            }).ToList();
-
-            // Build model
-            model.TotalSignups = formattedSignups.Count();
-            model.TotalMembers = formattedSignups.Select(s => s.UserId).Distinct().Count();
-            model.Semester = semester.ToString();
-            model.StartDate = ConvertUtcToCst(semester.DateStart).ToShortDateString();
-            model.EndDate = ConvertUtcToCst(semester.DateEnd).ToShortDateString();
-
-            // Calculations
-            var values = formattedSignups
-                .GroupBy(s => s.UserId)
-                .Select(s => new { UserId = s.Key, Value = s.Count() })
-                .OrderByDescending(s => s.Value)
-                .ToList();
-            model.ChartXValues = values.Select(s => s.Value).ToList();
-            model.ChartXLabels = new List<string>();
-            for (var i = 0; i < values.Count; i++)
-            {
-                if (signups.First(s => s.UserId == values[i].UserId).Member.WasLivingInHouse(semester.DateEnd))
-                {
-                    model.ChartXLabels.Add((i+1) + "*");
-                }
-                else
-                {
-                    model.ChartXLabels.Add((i+1).ToString());
-                }
-            }
-
-            var json = JsonConvert.SerializeObject(model, Formatting.Indented);
-            return Json(json, JsonRequestBehavior.AllowGet);
-        }
-
-        private static IEnumerable<string> GetDayNames()
-        {
-            if (CultureInfo.CurrentCulture.Name.StartsWith("en-"))
-            {
-                return new[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
-            }
-            return CultureInfo.CurrentCulture.DateTimeFormat.DayNames;
-        }
-
-        private static IEnumerable<DateTime> MonthsBetween(DateTime d0, DateTime d1)
-        {
-            return Enumerable.Range(0, (d1.Year - d0.Year) * 12 + (d1.Month - d0.Month + 1))
-                             .Select(m => new DateTime(d0.Year, d0.Month, 1).AddMonths(m));
-        }
-
-        private static int GetIso8601WeekOfYear(DateTime time)
-        {
-            // Seriously cheat.  If its Monday, Tuesday or Wednesday, then it'll 
-            // be the same week# as whatever Thursday, Friday or Saturday are,
-            // and we always get those right
-            DayOfWeek day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
-            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
-            {
-                time = time.AddDays(3);
-            }
-
-            // Return the week of our adjusted day
-            return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
-        } 
     }
 }
